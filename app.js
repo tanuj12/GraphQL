010059d96dccc9ec0f1360d9dc074eb8409749a4
Tanuj Kulkarni <tanuj21199@gmail.com>
2020-11-18 18:57:22 +0530

    Converted a messaging application to graphql

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const multer = require('multer');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+
+const clearImage = require('./util/file');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -34,8 +41,45 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
+
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+    if(!req.isAuth) {
+        const error = new Error('Not Authenticated');
+        error.code = 401;
+        throw error;
+    }
+    if(!req.file) {
+        return res.status(200).json({message: "No file provided"});
+    }
+    if (req.body.oldPath) {
+        clearImage(req.body.oldPath);
+    }
+    return res.status(201).json({message: 'File stored', filePath: req.file.path.replace(/\\/g, '/')})
+})
+
+
+
+app.use('/graphql', graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+        if (!err.originalError) {
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || "An error occured";
+        const code = err.originalError.code || 500;
+        return {message, status: code, data}
+    }
+}));
 
 app.use((error, req,res,next) => {
     console.log(error);
@@ -44,10 +88,11 @@ app.use((error, req,res,next) => {
     const data = error.data
     res.status(status).json({message, data});
 })
-mongoose.connect('Your mongoDb uri')
+mongoose.connect('Your mongodb URI')
 .then(result => {
     app.listen(8080);
 })
 .catch(err => {
     console.log(err)
 })
+
